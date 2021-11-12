fbf989e30717096e7bae3c0730cd71aa955cd87c
FGH21marha <martin.halldin@futuregames.nu>
2021-11-12 16:15:59 +0100

    fonts!

--- Honk Vs Bonk/Assets/PlayerController.cs
+++ Honk Vs Bonk/Assets/PlayerController.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera cam;
+    private Rigidbody rb;
+    private NavMeshAgent agent;
+
+    private void Awake()
     {
-        
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 }
